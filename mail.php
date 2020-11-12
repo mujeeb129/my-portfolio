63b1a3a309eeeb440bcddc4891101c0a19617c7b
Mujeeb Rahman <mujeebmj313@gmail.com>
2020-11-12 19:43:42 +0530

    Making contact form

--- mail.php
+++ mail.php
@@ -1,8 +1,9 @@
-<?php $name = $_POST['name'];
+<?php 
+$name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['describtion'];
+$message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "mujeebmj@gmail.com";
+$recipient = "mujeebmj313@gmail.com";
 $subject = $_POST['subject'];
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
