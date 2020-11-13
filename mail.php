ebb0fa74319e027c5a3b30ca03921733102f752d
Mujeeb Rahman <mujeebmj313@gmail.com>
2020-11-13 15:24:23 +0530

    Image size reduced and mail check1

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "mujeebmj313@gmail.com";
+$recipient = "her-rider@yandex.com";
 $subject = $_POST['subject'];
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
